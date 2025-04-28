b760726ac6dab364088f8539998b4a4bfdd2bcbb
Alex <126229845+Fjurn01@users.noreply.github.com>
2025-04-28 23:27:55 +0300

    Added Controller Logic

---
+++ EventPlanner/EventPlanner/Controllers/HostController.cs
@@ -0,0 +1,86 @@
+﻿using EventPlanner.Repository.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class HostController : Controller
+{
+  private readonly IRepositoryWrapper _repo;
+
+  public HostController(IRepositoryWrapper repo)
+  {
+    _repo = repo;
+  }
+
+  public async Task<IActionResult> Index()
+  {
+    var hosts = await _repo.Host.GetAllAsync();
+    return View(hosts);
+  }
+
+  public async Task<IActionResult> Details(int id)
+  {
+    var host = await _repo.Host.GetByIdAsync(
+    e => e.HostId == id,
+    include: q => q.Include(e => e.EventsHosted)
+);
+
+    if (host == null) return NotFound();
+    return View(host);
+  }
+  [Authorize(Roles = "Admin")]
+  public IActionResult Create() => View();
+  [Authorize(Roles = "Admin")]
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public async Task<IActionResult> Create(EventPlanner.Models.Host host)
+  {
+    if (ModelState.IsValid)
+    {
+      await _repo.Host.AddAsync(host);
+      await _repo.SaveAsync();
+      return RedirectToAction(nameof(Index));
+    }
+    return View(host);
+  }
+  [Authorize(Roles = "Admin")]
+  public async Task<IActionResult> Edit(int id)
+  {
+    var host = await _repo.Host.GetByIdAsync(id);
+    if (host == null) return NotFound();
+    return View(host);
+  }
+  [Authorize(Roles = "Admin")]
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public async Task<IActionResult> Edit(int id, EventPlanner.Models.Host host)
+  {
+    if (id != host.HostId) return NotFound();
+
+    if (ModelState.IsValid)
+    {
+      _repo.Host.Update(host);
+      await _repo.SaveAsync();
+      return RedirectToAction(nameof(Index));
+    }
+    return View(host);
+  }
+  [Authorize(Roles = "Admin")]
+  public async Task<IActionResult> Delete(int id)
+  {
+    var host = await _repo.Host.GetByIdAsync(id);
+    _repo.Host.Delete(host);
+    await _repo.SaveAsync();
+    return RedirectToAction(nameof(Index));
+  }
+  [Authorize(Roles = "Admin")]
+  [HttpPost, ActionName("Delete")]
+  [ValidateAntiForgeryToken]
+  public async Task<IActionResult> DeleteConfirmed(int id)
+  {
+    var host = await _repo.Host.GetByIdAsync(id);
+    _repo.Host.Delete(host);
+    await _repo.SaveAsync();
+    return RedirectToAction(nameof(Index));
+  }
+}
--- EventPlanner/EventPlanner/Controllers/RegistrationController.cs
+++ EventPlanner/EventPlanner/Controllers/RegistrationController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EventPlanner.Models;
+using EventPlanner.Repository.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace EventPlanner.Controllers
+[Authorize]
+public class RegistrationController : Controller
 {
-    public class RegistrationController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+  private readonly IRepositoryWrapper _repo;
+  private readonly UserManager<ApplicationUser> _userManager;
+
+  public RegistrationController(IRepositoryWrapper repo, UserManager<ApplicationUser> userManager)
+  {
+    _repo = repo;
+    _userManager = userManager;
+  }
+
+  public async Task<IActionResult> Index()
+  {
+    var user = await _userManager.GetUserAsync(User);
+    if (user == null) return Unauthorized();
+
+    var userRegistrations = await _repo.Registration.GetAllAsync(
+        filter: r => r.UserId == user.Id,
+        include: r => r.Include(r => r.Event)
+    );
+
+    return View(userRegistrations);
+  }
+
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public async Task<IActionResult> Unregister(int id)
+  {
+    var reg = await _repo.Registration.GetByIdAsync(id);
+    if (reg == null) return NotFound();
+
+    _repo.Registration.Delete(reg);
+    await _repo.SaveAsync();
+
+    return RedirectToAction(nameof(Index));
+  }
 }
